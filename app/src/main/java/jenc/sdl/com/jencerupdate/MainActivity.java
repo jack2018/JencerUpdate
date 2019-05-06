a6f380f4c2d48f400029bc1cf7e242d57d76cb68
jack2018 <610184089@qq.com>
2019-05-06 17:02:37 +0800

    Initial commit

---
+++ app/src/main/java/jenc/sdl/com/jencerupdate/MainActivity.java
@@ -0,0 +1,13 @@
+package jenc.sdl.com.jencerupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
